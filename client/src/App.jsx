4cfbb50e782a0142f097c35d0a1ea4fb1418f06a
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-09 15:12:54 -0400

    (init)

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Sedna</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const OUT_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  // Resolve sass extension? @import "bootstrap";
+  // Do we need bootstrap?
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};
