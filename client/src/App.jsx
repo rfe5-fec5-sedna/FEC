bdf6528309fb564dfdb72091601b292dae3acfe2
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-09-10 19:03:04 -0400

    "Proxy Server Set Up"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  ComponentDidMount() {
+    axios.get('/sedna/products')
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { API_TOKEN } = require('./config.js');
+
 const app = express();
-
-const { router } = require('./routes.js')
-
 const PORT = process.env.PORT || 3000;
 
-app.use(router);
+
+const API_SERVICE_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe'
+const apiHeader = {
+  'Authorization': API_TOKEN
+}
+
+app.use('/sedna', createProxyMiddleware({
+  headers: apiHeader,
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/sedna`]: '',
+  },
+}));
 
 app.listen(PORT, () => {
   console.log(`Sedna test server listening at http://localhost:${PORT}`);
 })
+
+
