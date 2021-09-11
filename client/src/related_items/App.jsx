f8a9ae305cb875501e444c1fec5cdec08ffbd549
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-11 18:45:59 -0400

    Create basic Related Items component

--- client/src/related_items/App.jsx
+++ client/src/related_items/App.jsx
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const RelatedItems = (props) => {
+  const currentProduct = props.currentProduct;
+
+  return (
+    <div className="Related-Items">
+      <h1>Related Items</h1>
+      <h3>{currentProduct}</h3>
+    </div>
+  )
+}
+
+export default RelatedItems;
