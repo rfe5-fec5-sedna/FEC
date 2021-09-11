564b80238e69c34fc9efaed52b9dbeb30d6075ab
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-11 16:43:43 -0400

    "Set basic App state"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import axios from 'axios';
 
+import SearchBar from './SearchBar.jsx';
+
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      currentProduct: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  ComponentDidMount() {
-    axios.get('/products/1/styles')
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.error(error);
-    })
+  handleClick(e, id) {
+    e.preventDefault();
+    this.setState({ currentProduct: Number(id) })
+  }
+
+  componentDidMount() {
+    axios.get('/sedna/products')
+      .then((response) => {
+        this.setState({
+          currentProduct: response.data[0].id
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   }
 
   render() {
     return (
       <div>
         <h1>Hello Sedna</h1>
+        <SearchBar handleClick={this.handleClick} />
       </div>
     )
   }
