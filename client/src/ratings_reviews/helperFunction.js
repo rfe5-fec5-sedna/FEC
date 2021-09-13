0cf8adfdda54f3f023b51f8595b6c882a6d25502
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-13 19:06:59 -0400

    Use hardcoded product id to test if helper function is working.

--- client/src/ratings_reviews/helperFunction.js
+++ client/src/ratings_reviews/helperFunction.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const getAllData = {
   getAllReviews: (id) => {
-    return axios.get(`/sedna/reviews/?product_id=${id}`)
+    // return axios.get(`/sedna/reviews/?product_id=${id}`)
+    return axios.get(`/sedna/reviews/?product_id=37311`)
   }
 }
 
