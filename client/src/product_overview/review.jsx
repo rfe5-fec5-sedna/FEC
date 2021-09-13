c791a144015856e6510d49897a25c91e8fb0dcf9
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-09-12 20:45:10 -0400

    "Add rating and total number of reviews"

---
+++ client/src/product_overview/review.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import api from './api.js';
+import Rating from 'react-rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as starSolid } from '@fortawesome/free-solid-svg-icons'
+import { faStar as starOutline } from '@fortawesome/free-regular-svg-icons'
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.solid = <FontAwesomeIcon icon={starSolid} />
+    this.outline = <FontAwesomeIcon icon={starOutline} />
+
+    this.state = {
+      count: '',
+      rating: ''
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.id !== prevProps.id) {
+      console.log(this.props)
+      api.getReview(this.props.id, (error, result) => {
+      if(error) {
+        console.log(error);
+      } else {
+        this.setState({
+          rating: api.starCalc(result)
+        })
+      }
+    })
+    };
+
+    if(this.props.id !== prevProps.id) {
+      api.getCount(this.props.id, (error, result) => {
+      if(error) {
+        console.log(error);
+      } else {
+        this.setState({
+          count: result
+        })
+      }
+    })
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Rating  emptySymbol={this.outline} fullSymbol={this.solid}  initialRating={this.state.rating} readonly/>
+        <div>Read all {this.state.count} reviews</div>
+      </div>
+    )
+  }
+};
+
+
+export default Review;
