8ea38b438170182a16f4a39decdb8a892f723669
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-13 19:21:47 -0400

    create basic outfit component

---
+++ client/src/related_items/Outfit.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Card from './Card';
+import './styles/YourOutfit.css'
+
+const Outfit = (props) => {
+
+  return (
+    <div id="Outfit">
+      <h1 id="your-outfit-header">Your Outfit</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default Outfit;
