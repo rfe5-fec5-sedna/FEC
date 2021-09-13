d3131e8d1b1845e5f66265cffa7d1acad88b8bce
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-09-13 14:13:39 -0400

    "Using componentDidUpdate to update product_overview level state"

--- client/src/product_overview/App.jsx
+++ client/src/product_overview/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import api from './api.js';
 import Review from './review.jsx';
+import Product_Info from './product_info.jsx';
+import Image_Gallery from './image.jsx';
+import Style from './style_selector.jsx';
 
 
 class Overview extends React.Component {
@@ -7,15 +11,45 @@ class Overview extends React.Component {
     super(props)
 
     this.state = {
-      placeholder: ''
+      category: '',
+      title: '',
+      slogan: '',
+      description: '',
+      styles: [],
+      currentStyle: '',
+      currentStyleSkus: [],
+      cart: {},
+      photos: []
     }
   }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.id !== prevProps.id) {
+      api.getProduct(this.props.id, (error, result) => {
+      if(error) {
+        console.log(error);
+      } else {
+        this.setState({
+          category: result.category,
+          title: result.name,
+          slogan: result.slogan,
+          description: result.description
+        })
+      }
+    })
+    };
+  }
+
 
   render() {
     return(
       <div>
-         <h2>-----------------Product Overview---------------------{this.props.id}</h2>
+         <h2>-----------------Product Overview---------------------</h2>
+         <Image_Gallery photos={this.state.photos} />
          <Review id={this.props.id} />
+         <Product_Info category={this.state.category} title={this.state.title}
+            slogan={this.state.slogan} description={this.state.description} />
+         <Style styles={this.state.styles} />
       </div>
     )
   }
