05b07779ad9a69150048cc005929a32371ef491f
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-13 19:22:16 -0400

    create component that renders related products and outfit components

---
+++ client/src/related_items/RelatedItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import RelatedProducts from './RelatedProducts';
+import Outfit from './Outfit';
+import './styles/RelatedItems.css';
+
+const RelatedItems = (props) => {
+  const currentProductId = props.currentProductId;
+
+  return (
+    <div id="Related-Items">
+      <RelatedProducts currentProductId={currentProductId} />
+      <Outfit />
+    </div>
+  )
+}
+
+export default RelatedItems;
