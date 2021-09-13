32084961814ec0f8e1cc448d28d13109beb9878b
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-13 15:47:04 -0400

    Change class component to functioncal component, set currentProduct variable.

--- client/src/ratings_reviews/App.jsx
+++ client/src/ratings_reviews/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReviewsList from './ReviewsList.jsx';
 
-class RatingsReviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentProductId: props.currentProduct
-    }
-  }
+const RatingsReviews = (props) => {
+  let currentProduct = props.currentProduct;
 
-  render() {
-    return (
-      <div id="ratings-reviews">
-        <h1>|-----------Ratings and Reviews ----------|</h1>
-        <ReviewsList currentProduct={this.props.currentProduct}/>
-      </div>
-    )
-  }
+  return (
+    <div id="ratings-reviews">
+      <h1>|-----------Ratings and Reviews ----------|</h1>
+      <ReviewsList currentProduct={currentProduct}/>
+    </div>
+  )
 }
 
 export default RatingsReviews;
