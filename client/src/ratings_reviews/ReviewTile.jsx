f10af6caa5a20967a6ba3720165b75cd4ab4f8d6
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-13 14:55:54 -0400

    Add placeholders for firsst and second individual reviews.

--- client/src/ratings_reviews/ReviewTile.jsx
+++ client/src/ratings_reviews/ReviewTile.jsx
@@ -4,7 +4,9 @@ let ReviewTile = (props) => {
   return (
     <div id="review-tile">
       <h3>Individual Review Tile</h3>
-      <h5>{props.productId}</h5>
+      <h5>Current Product ID: {props.productId}</h5>
+      <h5>Review #1</h5>
+      <h5>Review #2</h5>
     </div>
   );
 }
