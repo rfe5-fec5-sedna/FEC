a7ea81188aca160d7646f75efe964d27218b4d0b
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-14 18:18:50 -0400

    define getRelated and getProductsData helper functions

--- client/src/related_items/helpers.js
+++ client/src/related_items/helpers.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
 
 const helpers = {
-  getRelated: (id) => {
-    const url = `sedna/products/${id}/related`;
-    return axios.get(url);
+  getRelated: async (id) => {
+    const url = `/sedna/products/${id}/related`;
+    const products = await axios.get(url);
+    return products;
+  },
+  getProductsData: async (relatedProducts) => {
+    const relatedProductsData = [];
+    for await (let productId of relatedProducts) {
+      const url = `/sedna/products/${productId}`;
+      try {
+        const data = await axios.get(url);
+        relatedProductsData.push(data.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return relatedProductsData
   }
 }
 
