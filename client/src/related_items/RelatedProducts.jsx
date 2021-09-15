2ea581c065fb03f227e0d42fe338cce21a1a2296
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-14 22:02:11 -0400

    use Hooks to manage state

--- client/src/related_items/RelatedProducts.jsx
+++ client/src/related_items/RelatedProducts.jsx
@@ -1,53 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Card from './Card';
 import helpers from './helpers';
 import './styles/RelatedProducts.css'
 
-class RelatedProducts extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      relatedProducts: [],
-    }
-  }
+const RelatedProducts = (props) => {
+  const currentId = props.currentProductId;
 
-  componentDidUpdate(prevProps) {
-    if (this.props.currentProductId !== prevProps.currentProductId) {
-      helpers.getRelated(this.props.currentProductId)
-        .then((res) => {
-          const relatedProducts = res.data;
-          return relatedProducts
-        })
-        .then((products) => {
-          helpers.getProductsData(products)
-            .then((res) => {
-              this.setState({ relatedProducts: res })
-            })
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-    }
-  }
+  const [productId, setProductId] = useState(currentId);
+  const [relatedProducts, setRelatedProducts] = useState([]);
 
-  render() {
-    return (
-      <div id="Related-Products" >
-        <h1 id="related-product-header">Related Products</h1>
-        <div>
-          {this.state.relatedProducts.map((product, index) => {
-            return (<Card
-              productName={product.name}
-              productCategory={product.category}
-              productPrice={product.default_price}
-            />)
-          })}
-        </div>
+  useEffect(() => {
+    setProductId(currentId)
+  }, [currentId]);
+
+  useEffect(() => {
+    helpers.getRelated(productId)
+      .then((res) => {
+        setRelatedProducts(res);
+      })
+  }, [productId])
+
+  return (
+    <div id="Related-Products" >
+      <h1 id="related-product-header">Related Products</h1>
+      <div className="related-card">
+        {relatedProducts.map(productId => (
+          <Card key={productId} productId={productId} />
+        ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default RelatedProducts;
