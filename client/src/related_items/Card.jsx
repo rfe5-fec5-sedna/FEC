264bca01a8bfdbe2b56c5986268b81c5412d7e79
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-14 22:01:35 -0400

    render name, category, price and image of product

--- client/src/related_items/Card.jsx
+++ client/src/related_items/Card.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import helpers from './helpers';
 import './styles/Card.css';
 
 const Card = (props) => {
+  const productId = props.productId;
+
+  const [name, setName] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    helpers.getProductData(productId)
+      .then(res => {
+        setName(res.name);
+        setCategory(res.category);
+        setPrice(res.default_price);
+      })
+  }, [productId])
+
+  useEffect(() => {
+    helpers.getProductImage(productId)
+      .then(res => {
+        setImage(res.results[0].photos[0].thumbnail_url)
+      })
+  }, [productId])
+
   return (
     <div className="Card-Component">
-      <div className="product-name">{props.productName}</div>
-      <div className="product-category">{props.productCategory}</div>
-      <div className="product-price">{props.productPrice}</div>
+      <img className="product-image" src={image} />
+      <h5 className="product-name">{name}</h5>
+      <h5 className="product-category">{category}</h5>
+      <h5 className="product-price">{price}</h5>
     </div>
   )
 }
