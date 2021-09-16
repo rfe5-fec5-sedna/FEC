101745d9deb12dc14b16116dd29c36143565f921
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-16 13:29:37 -0400

    Fix a bug where the date format was creating the current date instead of taking in the review date. Error fixed.

--- client/src/ratings_reviews/helperFunction.js
+++ client/src/ratings_reviews/helperFunction.js
@@ -22,9 +22,9 @@ const getAllData = {
     }
   },
 
-  dateFormat: () => {
+  dateFormat: (reviewDate) => {
     let monthNames = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    let date = new Date();
+    let date = new Date(reviewDate);
     let currentDay = date.getDate();
     let currentMonth = date.getMonth() + 1;
     let currentYear = date.getFullYear();
