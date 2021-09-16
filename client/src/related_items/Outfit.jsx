f04d80ad36294a374d3fa1faa8ca719c6243e1d8
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-16 19:13:34 -0400

    add new product card for product added to outfit carousel

--- client/src/related_items/Outfit.jsx
+++ client/src/related_items/Outfit.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from './Card';
 import './styles/OutfitCarousel.css'
 
-const Outfit = (props) => {
+const OutfitProducts = ({ currentProductId }) => {
+
+  const [outfits, setOutfits] = useState([])
+
+  const handleClick = () => {
+    if (outfits.includes(currentProductId)) return
+    setOutfits(outfits => [...outfits, currentProductId])
+  }
 
   return (
     <div id="Outfit">
       <h1 id="your-outfit-header">Your Outfit</h1>
-      <Card />
+      <div id="empty-outfit-card" onClick={handleClick}>
+        <p>Add this to your Outfit!</p>
+      </div>
+      <div id="outfit-cards">
+        {outfits.reverse().map(productId => (
+          <Card key={productId} productId={productId} />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Outfit;
+export default OutfitProducts;
