e86a4859f72bda8c925be605979da22e0808bc4b
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-16 17:19:41 -0400

    display arrows and implement carousel logic and functionality

--- client/src/related_items/RelatedProducts.jsx
+++ client/src/related_items/RelatedProducts.jsx
@@ -10,23 +10,44 @@ const RelatedProducts = (props) => {
 
   const [productId, setProductId] = useState(currentId);
   const [relatedProducts, setRelatedProducts] = useState([]);
+  const [firstDisplayed, setFirstDisplayed] = useState(0);
+  const [lastDisplayed, setLastDisplayed] = useState(3);
+
+  const productsAmount = relatedProducts.length;
+
+  const handleBackward = () => {
+    setFirstDisplayed(firstDisplayed - 1);
+    setLastDisplayed(lastDisplayed - 1);
+  }
+
+  const handleForward = () => {
+    setLastDisplayed(lastDisplayed + 1);
+    setFirstDisplayed(firstDisplayed + 1);
+  }
 
   useEffect(() => {
     setProductId(currentId)
   }, [currentId]);
 
-  // useEffect(() => {
-  //   helpers.getRelated(productId)
-  //     .then((res) => {
-  //       setRelatedProducts(res);
-  //     })
-  // }, [productId])
+  useEffect(() => {
+    helpers.getRelated(productId)
+      .then((res) => {
+        setRelatedProducts(res);
+      })
+  }, [productId])
+
+  const displayProducts = (productsAmount > 3) ? relatedProducts.slice(firstDisplayed, lastDisplayed) : relatedProducts;
+
+  console.log('related products => ', relatedProducts)
+  console.log('products displayed => ', displayProducts)
 
   return (
     <div id="related-products" >
       <h1 id="related-product-header">Related Products</h1>
       <div id="related-cards">
-        {relatedProducts.map(productId => (
+        {firstDisplayed !== 0 && <a onClick={handleBackward}>&#10094;</a>}
+        {lastDisplayed !== productsAmount && <a onClick={handleForward}>&#10095;</a>}
+        {displayProducts.map(productId => (
           <Card key={productId} productId={productId} />
         ))}
       </div>
@@ -35,3 +56,4 @@ const RelatedProducts = (props) => {
 }
 
 export default RelatedProducts;
+
