955a1bf77a896ed86dda0f8ef15e391abf635ca2
Sebastian Carazo <sebastian.hackreactor@gmail.com>
2021-09-17 10:15:22 -0400

    set up a basic outfit carousel

--- client/src/related_items/Outfit.jsx
+++ client/src/related_items/Outfit.jsx
@@ -6,11 +6,23 @@ import './styles/OutfitCarousel.css'
 const OutfitProducts = ({ currentProductId }) => {
 
   const [outfits, setOutfits] = useState([])
+  const [firstDisplayed, setFirstDisplayed] = useState(0);
+  const [lastDisplayed, setLastDisplayed] = useState(3);
+
+  const handleBackward = () => {
+    // TODO: FIX LOGIC FOR OUTFIT CAROUSEL
+  }
+
+  const handleForward = () => {
+    // TODO: FIX LOGIC FOR OUTFIT CAROUSEL
+  }
 
   const handleClick = () => {
     if (outfits.includes(currentProductId)) return
     setOutfits(outfits => [...outfits, currentProductId])
   }
+
+  const displayProducts = (outfits.length > 3) ? outfits.slice(firstDisplayed, lastDisplayed) : outfits;
 
   return (
     <div id="Outfit">
@@ -18,8 +30,10 @@ const OutfitProducts = ({ currentProductId }) => {
       <div id="empty-outfit-card" onClick={handleClick}>
         <p>Add this to your Outfit!</p>
       </div>
-      <div id="outfit-cards">
-        {outfits.reverse().map(productId => (
+      <div id="outfit-products">
+        {firstDisplayed !== 0 && <a id="left-arrow" onClick={handleBackward}>&#10094;</a>}
+        {outfits.length > 3 && <a id="right-arrow" onClick={handleForward}>&#10095;</a>}
+        {displayProducts.reverse().map(productId => (
           <Card key={productId} productId={productId} />
         ))}
       </div>
