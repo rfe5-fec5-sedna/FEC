9e765a6895f5f13a0bcbe7ec9a970b12091d4884
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-21 15:54:08 -0400

    Begin writing first test for Review Tile functionality.

---
+++ client/src/ratings_reviews/tests/review_tile.test.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import React from 'react';
+ import {cleanup, render} from '@testing-library/react';
+ import ReviewTile from '../ReviewTile.jsx';
+ import "@testing-library/jest-dom";
+
+ afterEach(cleanup);
+
+ it(
+   describe()
+ )
