f115d36447fa3362fbc7cfc85149f77de31e19b3
Shuang (Luna) Zheng <lunazhengs1216@gmail.com>
2021-09-22 14:21:42 -0400

    Remove console log

--- client/src/product_overview/api.js
+++ client/src/product_overview/api.js
@@ -41,7 +41,7 @@ const api = {
     const url = `/sedna/products/${id}`;
     axios.get(url)
       .then((res) => {
-        console.log('this is product res', res)
+        // console.log('this is product res', res)
         callback(null, res.data);
       })
       .catch((error) => {
@@ -54,7 +54,7 @@ const api = {
     const url = `/sedna/products/${id}/styles`;
     axios.get(url)
       .then((res) => {
-        console.log('this is style res', res)
+        // console.log('this is style res', res)
         callback(null, res.data.results);
       })
       .catch((error) => {
