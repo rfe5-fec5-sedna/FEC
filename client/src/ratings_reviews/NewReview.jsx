7464b07d6d946f01ff41d2eb3e3ccfe82ab512f6
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-21 20:28:12 -0400

    Add conditional rendering for New Review component, add state properties to NewReview, add buttons, and handlers to NewReview and ReviewsList.

--- client/src/ratings_reviews/NewReview.jsx
+++ client/src/ratings_reviews/NewReview.jsx
@@ -1,62 +1,70 @@
-import React from 'react';
-import helperFunction from './helperFunction.js';
-import './styles/NewReview.css';
+import React from "react";
+import helperFunction from "./helperFunction.js";
+import "./styles/NewReview.css";
 
 class NewReview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product_id: this.props.productId,
-      overallRating: '',
-      productRecommend: '',
+      overallRating: "",
+      productRecommend: "",
       characteristics: {},
-      reviewSummary: '',
-      reviewBody: '',
+      reviewSummary: "",
+      reviewBody: "",
       photos: [],
-      nickname: '',
-      email: '',
-      submitReview: ''
-    }
+      nickname: "",
+      email: "",
+      submitReview: "",
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
-
-
-
-
   handleSubmit(e) {
-    console.log('Submit Review clicked')
+    console.log("Submit Review clicked");
     e.preventDefault();
-    helperFunction.postNewReview(this.props.currentProductId)
-        .then((response) => {
-            console.log(response);
-            // this.setState({
-            //   reviewsList: allReviews,
-            //   allReviewsDisplayed: true
-            // })
-          })
-        .catch(err => {
-          console.log(err);
-        })
+    helperFunction.postNewReview(this.props.productId)
+      .then((response) => {
+        console.log(response);
+        // this.setState({
+        //   reviewsList: allReviews,
+        //   allReviewsDisplayed: true
+        // })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-
-
-
-
-
-
-
 
   render() {
     return (
       <div id="new-review-modal">
         <div id="new-review-modal-header">
+          <h1>Write Your Review</h1>
+          <span className="new-review-x-button" onClick={this.props.closeNewReview}>
+            x
+          </span>
+        </div>
+        <div id="new-review-modal-body">
+          <h3>About the {this.props.productId}</h3>
+        </div>
+        <form>
+          <button
+            type="submit"
+            id="submit-review-button"
+            onClick={this.handleSubmit}
+          >
+            SUBMIT REVIEW
+          </button>
+        </form>
+        <div id="new-review-modal-footer">
           <form>
-            {/* <button type="submit" id="submit-review-button" onClick={this.handleSubmit}>
-              SUBMIT REVIEW
-            </button> */}
+            <button
+              type="submit"
+              id="close-review-button"
+              onClick={this.props.closeNewReview}
+            >
+              CLOSE
+            </button>
           </form>
         </div>
       </div>
--- client/src/ratings_reviews/ReviewsList.jsx
+++ client/src/ratings_reviews/ReviewsList.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReviewTile from './ReviewTile.jsx';
-import NewReview from './NewReview.jsx';
-import helperFunction from './helperFunction.js';
-import './styles/ReviewsList.css';
+import React from "react";
+import ReviewTile from "./ReviewTile.jsx";
+import NewReview from "./NewReview.jsx";
+import helperFunction from "./helperFunction.js";
+import "./styles/ReviewsList.css";
+import "./styles/NewReview.css";
 
 class ReviewsList extends React.Component {
   constructor(props) {
@@ -11,82 +12,82 @@ class ReviewsList extends React.Component {
       reviewsList: [],
       shortList: [],
       allReviewsDisplayed: false,
-      sortOption: 'relevant',
-      openModal: false,
-      modalDisplayed: false
-    }
+      sortOption: "relevant",
+      openModal: false
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleMoreReviewsClick = this.handleMoreReviewsClick.bind(this);
     this.openNewReviewClick = this.openNewReviewClick.bind(this);
+    this.closeNewReviewClick = this.closeNewReviewClick.bind(this);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.currentProductId !== prevProps.currentProductId) {
       helperFunction.getAllReviews(this.props.currentProductId)
         .then((response) => {
-            let allReviews = response.data.results;
-            this.setState({
-              reviewsList: allReviews.slice(0, 2)
-            })
-          })
-        .catch(err => {
-          console.log(err);
+          let allReviews = response.data.results;
+          this.setState({
+            reviewsList: allReviews.slice(0, 2),
+          });
         })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
 
   handleChange(e) {
     let newSort = e.target.value;
     this.setState({
-      sortOption: newSort,
-    })
+      sortOption: newSort
+    });
     helperFunction.getAllReviewsWithSort(this.props.currentProductId, newSort)
       .then((response) => {
         let allReviewsWithSort = response.data.results;
         if (this.state.allReviewsDisplayed) {
           this.setState({
             reviewsList: allReviewsWithSort
-          })
+          });
         } else {
           this.setState({
             reviewsList: allReviewsWithSort.slice(0, 2)
-          })
+          });
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
+      });
   }
 
   handleMoreReviewsClick(e) {
     e.preventDefault();
     helperFunction.getAllReviews(this.props.currentProductId)
-        .then((response) => {
-            let allReviews = response.data.results;
-            this.setState({
-              reviewsList: allReviews,
-              allReviewsDisplayed: true
-            })
-          })
-        .catch(err => {
-          console.log(err);
-        })
+      .then((response) => {
+        let allReviews = response.data.results;
+        this.setState({
+          reviewsList: allReviews,
+          allReviewsDisplayed: true
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   openNewReviewClick(e) {
-    console.log('Open Modal clicked')
+    console.log("Open Modal clicked");
     e.preventDefault();
     this.setState({
-      openModal: true
-    })
+      openModal: true,
+    });
   }
 
   closeNewReviewClick(e) {
-    console.log('Close Modal clicked')
+    console.log("Close Modal clicked");
     e.preventDefault();
     this.setState({
-      openModal: false
-    })
+      openModal: false,
+    });
   }
 
   render() {
@@ -94,32 +95,61 @@ class ReviewsList extends React.Component {
       <div id="reviews-list">
         <div className="single-review-tile">
           <div id="sorting-option-header">
-            <h3>{helperFunction.reviewListLength(this.state.reviewsList, this.state.sortOption)}
-            <select value={this.state.sortOption} onChange={this.handleChange} id="sort-option-button">
-              <option value={"none"}>None</option>
-              <option value="relevant">Relevant</option>
-              <option value="newest">Newest</option>
-              <option value="helpful">Helpful</option>
-            </select>
+            <h3>
+              {helperFunction.reviewListLength(
+                this.state.reviewsList,
+                this.state.sortOption
+              )}
+              <select
+                value={this.state.sortOption}
+                onChange={this.handleChange}
+                id="sort-option-button"
+              >
+                <option value={"none"}>None</option>
+                <option value="relevant">Relevant</option>
+                <option value="newest">Newest</option>
+                <option value="helpful">Helpful</option>
+              </select>
             </h3>
           </div>
           {this.state.reviewsList.map((singleReview) => {
             return (
-              <ReviewTile key={singleReview.review_id} productId={this.props.currentProductId} singleReview={singleReview} />
+              <ReviewTile
+                key={singleReview.review_id}
+                productId={this.props.currentProductId}
+                singleReview={singleReview}
+              />
             );
           })}
           <div id="ratings-button-container">
             <div id="ratings-more-reviews">
               <form onClick={this.handleMoreReviewsClick}>
-                <button type="submit" id="more-reviews-button">MORE REVIEWS</button>
+                <button type="submit" id="more-reviews-button">
+                  MORE REVIEWS
+                </button>
               </form>
             </div>
             <div id="ratings-new-review">
-              <NewReview productId={this.props.currentProductId} modelDisplayed={this.state.modalDisplayed} closeNewReview={this.closeNewReviewClick} />
               <form onClick={this.openNewReviewClick}>
-                <button type="submit" id="add-review-button">ADD A REVIEW +</button>
+                <button type="submit" id="add-review-button">
+                  ADD A REVIEW +
+                </button>
               </form>
             </div>
+          </div>
+          <div id="ratings-new-review-modal">
+            {this.state.openModal && (
+              <div
+                onClick={this.closeNewReviewClick}
+                className="modal-background">
+                  Hello World!
+                <NewReview
+                  productId={this.props.currentProductId}
+                  openModal={this.state.openModal}
+                  closeNewReview={this.closeNewReviewClick}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
