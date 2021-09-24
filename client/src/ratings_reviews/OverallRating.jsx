56b0b5c62e4855001defad4481639c653c1c5ce4
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-23 22:00:51 -0400

    Complete overall rating star functionality.

--- client/src/ratings_reviews/OverallRating.jsx
+++ client/src/ratings_reviews/OverallRating.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import helperFunction from "./helperFunction.js";
-import Rating from 'react-rating';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+import Rating from "react-rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
 
 class OverallRating extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      rating: '',
       fiveStars: '',
       fourStars: '',
       threeStars: '',
       twoStars: '',
-      oneStar: ''
+      oneStar: '',
     };
+    this.handleStarClick = this.handleStarClick.bind(this);
+  }
+
+  handleStarClick(value) {
+    this.setState({
+      rating: value,
+    });
   }
 
   render() {
@@ -23,11 +31,16 @@ class OverallRating extends React.Component {
 
     return (
       <div className="overall-rating-stars">
-        <Rating emptySymbol={starOutline} fullSymbol={starSolid} />
+        <Rating
+          emptySymbol={starOutline}
+          fullSymbol={starSolid}
+          value={this.state.rating}
+          initialRating={this.state.rating}
+          onChange={this.handleStarClick}
+        />
       </div>
     );
   }
-
 }
 
 export default OverallRating;
