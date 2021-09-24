f8db01ac7d7ac878ff4709e121cdcaf40c3ce177
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-23 22:17:08 -0400

    Refactor component from class to functional stateless component, for optimization.

--- client/src/ratings_reviews/OverallRating.jsx
+++ client/src/ratings_reviews/OverallRating.jsx
@@ -1,46 +1,25 @@
 import React from "react";
-import helperFunction from "./helperFunction.js";
 import Rating from "react-rating";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
 
-class OverallRating extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: '',
-      fiveStars: '',
-      fourStars: '',
-      threeStars: '',
-      twoStars: '',
-      oneStar: '',
-    };
-    this.handleStarClick = this.handleStarClick.bind(this);
-  }
+const OverallRating = (props) => {
+  let starRating = props.starRating;
+  let starOutline = <FontAwesomeIcon icon={emptyStar} />;
+  let starSolid = <FontAwesomeIcon icon={fullStar} />;
 
-  handleStarClick(value) {
-    this.setState({
-      rating: value,
-    });
-  }
-
-  render() {
-    let starOutline = <FontAwesomeIcon icon={emptyStar} />;
-    let starSolid = <FontAwesomeIcon icon={fullStar} />;
-
-    return (
-      <div className="overall-rating-stars">
-        <Rating
-          emptySymbol={starOutline}
-          fullSymbol={starSolid}
-          value={this.state.rating}
-          initialRating={this.state.rating}
-          onChange={this.handleStarClick}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="overall-rating-stars">
+      <Rating
+        emptySymbol={starOutline}
+        fullSymbol={starSolid}
+        value={starRating}
+        initialRating={starRating}
+        onChange={props.onChange}
+      />
+    </div>
+  );
 }
 
 export default OverallRating;
